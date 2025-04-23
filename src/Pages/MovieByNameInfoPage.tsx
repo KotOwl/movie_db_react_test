3c78594bb8a965ef4f0f595057b1a9c63401d39b
KotOwl <stas.black31@gmail.com>
2025-04-23 22:41:49 +0300

    just few changes

--- src/Pages/MovieByNameInfoPage.tsx
+++
@@ -1,5 +0,0 @@
-export const MovieByNameInfoPage = () => {
-    return (
-        <></>
-    );
-};
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,7 +2,6 @@ import {createBrowserRouter} from "react-router";
 import {MainLayout} from "../Layouts/MainLayout.tsx";
 import {MoviesPage} from "../Pages/MoviesPage.tsx";
 import {MovieIdInfoPage} from "../Pages/MovieIdInfoPage.tsx";
-import {MovieByNameInfoPage} from "../Pages/MovieByNameInfoPage.tsx";
 import {SorryPage} from "../Pages/SorryPage.tsx";
 import {FoundedFilmPage} from "../Pages/FoundedFilmPage.tsx";
 import {AuthorizationLayout} from "../Layouts/AuthorizationLayout.tsx";
@@ -12,7 +11,6 @@ export const routes = createBrowserRouter([{
     path:'/',element:<MainLayout/>,children:[
         {index:true,element:<MoviesPage/>},
         {path:'movie/:id',element:<MovieIdInfoPage/>},
-        {path:'movie/:name',element:<MovieByNameInfoPage/>},
         {path:'sorry',element:<SorryPage/>},
         {path:'foundedFilm',element:<FoundedFilmPage/>}
     ]},
